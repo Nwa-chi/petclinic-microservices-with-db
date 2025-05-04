4caea616c5ea98dbac82246bbfe63256b8ef69cd
Nwa-chi <kennwachukz@gmail.com>
2025-05-04 18:32:19 +0000

    removed some annomalies on the push-dev-docker-images-to-ecr-sh script

--- jenkins/push-dev-docker-images-to-ecr.sh
+++ jenkins/push-dev-docker-images-to-ecr.sh
@@ -1,5 +1,5 @@
 # Provide credentials for Docker to login the AWS ECR and push the images
-aws ecr get-login-password --region "us-east-1"" | docker login --username AWS --password-stdin "314146305101.dkr.ecr.us-east-1.amazonaws.com/clarusway-repo/petclinic-app-dev"
+aws ecr get-login-password --region "us-east-1"" | docker login --username AWS --password-stdin ECR_REGISTRY="314146305101.dkr.ecr.us-east-1.amazonaws.com/clarusway-repo/petclinic-app-dev"
 docker push "${IMAGE_TAG_ADMIN_SERVER}"
 docker push "${IMAGE_TAG_API_GATEWAY}"
 docker push "${IMAGE_TAG_CONFIG_SERVER}"
